4d43a0ac96607e0c12c4b072c197d43354f91ef8
gryprilliams <gageryanprince@gmail.com>
2023-04-25 11:31:28 -0400

    made it navigatable by URL

--- app/app.js
+++ app/app.js
@@ -1,24 +1,24 @@
-import { changePageContent } from "../model/model.js";
+import * as Model from "../model/model.js";
 
 function initListeners() {
-    $("nav #links a").on("click", function (e) {
-
-        let btnID = e.currentTarget.id; 
-        console.log("click", btnID);
-        changePageContent(btnID);
-        
-    })
+  $("nav #links a").on("click", function (e) {
+    let btnID = e.currentTarget.id;
+    pageChange(btnID);
+  });
 }
 
-function listen() {
-    $("#tourpackage #tourslink a").on("click", function (e){
-        alert(e.currentTarget.id);
-        changePageContent(e.currentTarget.id)
-    })
+function initURLListener() {
+  $(window).on("hashchange", Model.changeRoutes);
+  Model.changeRoutes();
 }
 
-$(document).ready(function() {
-    initListeners();
-    listen();
-    changePageContent("home");
+$(document).ready(function () {
+  //   initListener();
+  console.log(Model.wordArray);
+  initURLListener();
 });
+
+// function pageChange(pageName) {
+//   changePageContent(pageName);
+//   console.log(pageName);
+// }
